4b4486d75fe15f1bf87659f13639e7097d90bf35
“httpini” <nicolas.pinol@hotmail.com>
2022-09-01 21:35:55 -0300

    Nico - Agregamos los productos a la base de datos que se comparte

--- green-world.sql
+++ green-world.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-09-2022 a las 19:26:58
+-- Tiempo de generación: 02-09-2022 a las 02:35:09
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -75,7 +75,22 @@ INSERT INTO `imagenes` (`id`, `path`) VALUES
 (10, 'image_id-1661970028770-490509761.jpeg'),
 (11, 'image_id-1662042081069-570011053.jpeg'),
 (14, 'image_id-1662043114616-548302625.jpg'),
-(15, 'product-image-1662043127888-552389870.jpeg');
+(15, 'product-image-1662043127888-552389870.jpeg'),
+(17, 'image_id-1662054538775-954088958.jpg'),
+(18, 'weed-pipes.jpg'),
+(19, 'celulosa.jpg'),
+(20, 'aceite-500mg-30ml-Caja-Front-copia.jpg'),
+(21, 'Cannabis-Power-Skin-Control-50g-Front.jpg'),
+(22, 'Aceite-DnSelva-30ml-Caja-Frontal.jpg'),
+(23, 'COCOB-300x300.png'),
+(24, 'Go-nitro-300x300.png'),
+(25, 'GO-WORMS-300x300.png'),
+(26, 'TERRAFERTIL-TURBA-20L.png'),
+(27, 'Lightmix-10.png'),
+(28, 'VERMICULITA-GO-GREEN.png'),
+(29, 'FILTRO-6-COOLER-B1.png'),
+(30, 'TOBERA-COOLER-4a4.png'),
+(31, 'MALLA-SCROG-SEGURIDAD-B.png');
 
 -- --------------------------------------------------------
 
@@ -93,6 +108,27 @@ CREATE TABLE `productos` (
   `color` varchar(50) DEFAULT NULL,
   `price` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `name`, `description`, `image_id`, `category_id`, `user_id`, `color`, `price`) VALUES
+(5, 'prueba', 'prueba', 17, 3, NULL, 'prueba', 12345),
+(6, 'Pipas de cristal', 'La Pipa de Cristal Champ High de 11 cm está fabricada en cristal de borosilicato de uso médico con alta resistencia térmica para conseguir una calada fresca y limpia. Cuenta con carb (carburador) para vaciar la pipa. Su forma redondeada resulta perfecta para llevar en un bolsillo.Disponible en colores azul, rosa, transparente y verde.', 18, 4, NULL, 'Dorado', 800),
+(7, 'Celulosa', 'Papel de celulosa Zeus de color naranja es un papel naranja fabricado a partir de celulosa 100% natural. Esta presentación de celulosa Zeus no produce ceniza ni tampoco necesita pegamento, ya que humedeciendo uno de sus extremos, el cigarrillo queda prolijamente cerrado. Las celulosas vienen de color Naranja lo que le da un toque estético único.', 19, 4, NULL, NULL, 300),
+(8, 'Aceite Cannabico', 'Aceite hidratante corporal que ayuda a la relajación.', 20, 3, NULL, NULL, 3500),
+(9, 'Crema de cannabis Kif Skin Control 60grs', 'Este es un producto que cuenta con todas las propiedades cosméticas del cannabis ya que contiene extracto de la flor, y por ende es un producto de última tendencia en el mercado nacional. Es una novedosa formulación cosmética con todos los beneficios del cannabis (CBD) y la caléndula para el cuidado de la piel, con propiedades antioxidantes, ayuda a combatir las manchas y marcas producidas por el acné para obtener una piel más sana y lúcida.', 21, 3, NULL, NULL, 1500),
+(10, 'Aceite Cannabico 30ml', 'El aceite de cannabis es a base de oliva extravirgen con extracto de cannabis full spectrum, contiene aproximadamente entre 2.75% de CBD y menos del 1% de THC.', 22, 3, NULL, NULL, 5000),
+(11, 'Top Veg', 'Top Crop es un fertilizante líquido completo rico en ácidos húmicos y fúlvicos así como en macro y micronutrientes solubles en agua.', 23, 2, NULL, NULL, 1000),
+(12, 'Go-nitro-300x300', 'Go Green Argentina es un bio-fertilizante totalmente orgánico y natural indicado para el periodo vegetativo de tus plantas, que representa una estable e inigualable fuente de nitrógeno «saludable».', 24, 2, NULL, NULL, 1000),
+(13, 'GO-WORMS-300x300', 'Go Green Argentina es un bio-fertilizante totalmente orgánico y natural indicado para el periodo vegetativo de tus plantas, que representa una estable e inigualable fuente de nitrógeno «saludable».', 25, 2, NULL, NULL, 1000),
+(14, 'Turba Musgo Sphagnum', 'La Turba de Musgo Sphagnum Fueguina de la empresa Terrafertil ofrece una buena retención de agua lo cual favorece la rápida absorción de los elementos nutricionales necesarios y el desarrollo radicular de las plantas. Aporta porosidad y baja el pH.', 26, 5, NULL, NULL, 2000),
+(15, 'LightMix', 'Nuestro Sustrato Premium LightMix de Go Green Argentina es un tipo de suelo de origen natural y orgánico que aporta todos los elementos necesarios a nuestras plantas y semillas para obtener resultados de alta calidad.', 27, 5, NULL, NULL, 2500),
+(16, 'Vermiculita', 'La Vermiculita de Go Green Argentina es un sustrato ideal para mezclas con turba, compost, fertilizantes orgánicos, fibra de coco, etc. Constituye un componente principal en el medio del cultivo para obtener un sustrato equilibrado con el fin de lograr una planta sana.', 28, 5, NULL, NULL, 2000),
+(17, 'Filtros Para Extractores Hydra', 'Filtro de carbón activado Go Green Argentina para reducción de olor de 6 Pulgadas, usados generalmente en armarios y carpas. Incluye una capa de carbón que reduce considerablemente los olores orgánicos. Diseñado para ser usados con extractores marca Hydra o similares de 6 pulgadas.', 29, 1, NULL, NULL, 800),
+(18, 'Toberas', 'Presenta esta tobera/ adaptador, diseñada especialmente y a medida para su utilización en coolers de 120. Este producto se encuentra preparado para ser conectado a conductos de 4 pulgadas con la finalidad de realizar extracción o intracción de aire.', 30, 1, NULL, NULL, 800),
+(19, 'Red Plástica Para Scrog', 'Malla apta RED SCROG Naranja/ Seguridad y Señalización – Una solución Super Económica!', 31, 1, NULL, NULL, 1500);
 
 -- --------------------------------------------------------
 
@@ -118,7 +154,8 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `name`, `lastname`, `email`, `cultivo`, `password`, `image_id`, `admin`, `product_id`) VALUES
 (1, 'Inosuke', 'Hashibira', 'ihashibira@gworld.com', 'outdoor', '$2a$10$dwIiGPKgYnE1VwscsmSCQeMUX2lDWkngpHns1fEp7WZj7vPgzrMye', 7, 1, NULL),
-(2, 'Eric', 'Zatel', 'ezatel@gworld.com', 'ninguna', '$2a$10$dwIiGPKgYnE1VwscsmSCQeMUX2lDWkngpHns1fEp7WZj7vPgzrMye', 6, 1, NULL);
+(2, 'Eric', 'Zatel', 'ezatel@gworld.com', 'ninguna', '$2a$10$dwIiGPKgYnE1VwscsmSCQeMUX2lDWkngpHns1fEp7WZj7vPgzrMye', 6, 1, NULL),
+(4, 'Nico', 'Pini', 'nico.pini@email.com', 'ninguna', '$2a$10$GxYV/YruCgUuQNxRRWJNd.lH1aMwuLjmCUmJQ4vVdhQZqUkLgc8r6', 6, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -190,19 +227,19 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuariosproductos`
