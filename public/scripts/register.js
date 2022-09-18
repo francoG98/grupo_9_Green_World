468728ae57847c743a9a1e0844fbc147ffb39cac
“httpini” <nicolas.pinol@hotmail.com>
2022-09-18 17:30:52 -0300

    Nico - validacion de imagen para register

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -48,6 +48,10 @@ inputs.lastname.addEventListener("input", function(e){
     let value = e.target.value
     let feed = document.querySelector("p.lastname")
     let msg = null
+    let checkIcon = document.querySelector("label.lastname i.fa-circle-check")
+    let notCheckIcon = document.querySelector("label.lastname i.fa-circle-xmark")
+    feed.style.display="block"
+
     if(validator.isEmpty(value)){
         msg = "El apellido no puede quedar vacío."
     } else if(!validator.isLength(value,{min:2})){
@@ -56,15 +60,28 @@ inputs.lastname.addEventListener("input", function(e){
     if(msg){
         feed.innerText = msg 
         feed.style.color="red"
+        checkIcon.style.display= "none"
+        notCheckIcon.style.display= "inline"
     } else{
         feed.innerText = "El campo Apellido es correcto"
         feed.style.color= "#57CC99"
+        checkIcon.style.display= "inline"
+        notCheckIcon.style.display= "none"
     }
+    inputs.lastname.addEventListener("blur",function(){
+        feed.style.display="none"
+    })
+    inputs.lastname.addEventListener("focus",function(){
+        feed.style.display="block"
+    })
 })
 inputs.email.addEventListener("input", function(e){ 
     let value = e.target.value
     let feed = document.querySelector("p.email")
     let msg = null
+    let checkIcon = document.querySelector("label.email i.fa-circle-check")
+    let notCheckIcon = document.querySelector("label.email i.fa-circle-xmark")
+
     if(validator.isEmpty(value)){
         msg = "El email no puede quedar vacío."
     } else if(!validator.isEmail(value)){
@@ -73,12 +90,60 @@ inputs.email.addEventListener("input", function(e){
     if(msg){
         feed.innerText = msg 
         feed.style.color="red"
+        checkIcon.style.display= "none"
+        notCheckIcon.style.display= "inline"
     } else{
         feed.innerText = "El campo Email es correcto"
         feed.style.color= "#57CC99"
+        checkIcon.style.display= "inline"
+        notCheckIcon.style.display= "none"
     }
+    inputs.email.addEventListener("blur",function(){
+        feed.style.display="none"
+    })
+    inputs.email.addEventListener("focus",function(){
+        feed.style.display="block"
+    })
 })
 //VALIDACION PARA IMAGENES
+inputs.avatar.addEventListener("change", function(e){
+    let file = e.target.files
+    console.log(file)
+    let type = file[0].type 
+    let validTypes=["image/jpeg","image/png","image/svg+xml"]
+    let feed = document.querySelector("p.avatar")
+    let msg = null
+    let checkIcon=document.querySelector("label.avatar i.fa-circle-check")
+    let notCheckIcon = document.querySelector("label.avatar i.fa-circle-xmark")
+    
+
+    if(!validTypes.includes(type)){
+        msg = "La imagen no tiene una extensión válida"
+    } else if(!file){
+        msg = null
+    }
+    if(msg){
+        feed.innerText= msg
+        feed.style.color = "red"
+        checkIcon.style.display= "none"
+        notCheckIcon.style.display= "inline"
+    }else{
+        feed.innerText= null
+        feed.style.color= "#57CC99"
+        checkIcon.style.display= "inline"
+        notCheckIcon.style.display= "none"
+    }
+    inputs.avatar.addEventListener("blur",function(){
+        feed.style.display="none"
+    })
+    inputs.avatar.addEventListener("focus",function(){
+        feed.style.display="block"
+    })
+})
+
+
+
+
 
 inputs.password.addEventListener("input", function(e){
     let value = e.target.value
@@ -86,6 +151,13 @@ inputs.password.addEventListener("input", function(e){
     let conFeed= document.querySelector("p.passConfirm")
     let confPass = inputs.passConfirm.value
     let msg = null
+    let checkIcon = document.querySelector("label.password i.fa-circle-check")
+    let notCheckIcon = document.querySelector("label.password i.fa-circle-xmark")
+
+    let confCheck=document.querySelector("label.passConfirm i.fa-circle-check")
+    let confXmark= document.querySelector("label.passConfirm i.fa-circle-xmark")
+
+
     let config = {
         minLength: 8,
         minLowercase: 1,
@@ -93,12 +165,17 @@ inputs.password.addEventListener("input", function(e){
         minNumbers: 1,
         minSymbols: 1,
         returnScore: false}
+
     if(value != confPass){
         conFeed.innerText="Las contraseñas deben coincidir"
         conFeed.style.color = "red"
+        confCheck.style.display="none"
+        confXmark.style.display="inline"
     } else{
         conFeed.innerText="Las contraseñas coinciden"
         conFeed.style.color = "#57CC99"
+        confCheck.style.display="inline"
+        confXmark.style.display="none"
     }    
     if(validator.isEmpty(value)){
         msg = "La contraseña no puede quedar vacía"
@@ -110,10 +187,20 @@ inputs.password.addEventListener("input", function(e){
     if(msg){
         feed.innerText= msg
         feed.style.color = "red"
+        checkIcon.style.display= "none"
+        notCheckIcon.style.display= "inline"
     } else{
         feed.innerText= "La contraseña es Segura"
         feed.style.color= "#57CC99"
+        checkIcon.style.display= "inline"
+        notCheckIcon.style.display= "none"
     }
+    inputs.password.addEventListener("blur",function(){
+        feed.style.display="none"
+    })
+    inputs.password.addEventListener("focus",function(){
+        feed.style.display="block"
+    })
 })
 
 inputs.passConfirm.addEventListener("input", function(e){
@@ -121,16 +208,29 @@ inputs.passConfirm.addEventListener("input", function(e){
     let feed = document.querySelector("p.passConfirm")
     let msg = null
     let pass = inputs.password.value
+    let checkIcon = document.querySelector("label.passConfirm i.fa-circle-check")
+    let notCheckIcon = document.querySelector("label.passConfirm i.fa-circle-xmark")
+    feed.style.display="block"
     if(value != pass){
         msg = "Las contraseñas deben coincidir"
     }
     if(msg){
         feed.innerText=msg
         feed.style.color = "red"
+        checkIcon.style.display= "none"
+        notCheckIcon.style.display= "inline"
     } else{
         feed.innerText="Las contraseñas coinciden"
         feed.style.color = "#57CC99"
+        checkIcon.style.display= "inline"
+        notCheckIcon.style.display= "none"
     }
+    inputs.passConfirm.addEventListener("blur",function(){
+        feed.style.display="none"
+    })
+    inputs.passConfirm.addEventListener("focus",function(){
+        feed.style.display="block"
+    })
 })
 
 
