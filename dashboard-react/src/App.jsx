31498123817e79a501497255a7ef443efe290aa8
ericzatel <ericzatel13@gmail.com>
2022-09-30 18:26:54 -0300

    Eric- empezando react

---
+++ dashboard-react/src/App.jsx
@@ -0,0 +1,14 @@
+import {  } from 'react'
+import './App.css'
+import { Routes, Route, Link} from "react-router-dom"
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
