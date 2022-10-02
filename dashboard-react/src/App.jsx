3215f9740ac9716b908d47bf8c41ac6eb8d61cc5
ericzatel <ericzatel13@gmail.com>
2022-10-02 15:05:21 -0300

    Eric - probando

--- dashboard-react/src/App.jsx
+++ dashboard-react/src/App.jsx
@@ -1,12 +1,15 @@
 import {  } from 'react'
 import './App.css'
 import { Routes, Route, Link} from "react-router-dom"
+import Product from "./components/Product"
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/products" element={<Product/>} ></Route>
+      </Routes>
     </div>
   )
 }
