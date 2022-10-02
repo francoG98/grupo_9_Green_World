ee3609480bbcffb5ff90b3d3b51054e5f2a6aa19
“httpini” <nicolas.pinol@hotmail.com>
2022-10-02 20:22:12 -0300

    arreglo

--- source/routes/apis/apiProduct.routes.js
+++ source/routes/apis/apiProduct.routes.js
@@ -1,7 +1,10 @@
 const {Router}= require("express")
 const router = Router()
-const{findLastFiveProducts, findAllProducts, findOneProduct} = require("../../controllers/apis/productReactApi")
+const{findLastFiveProducts, findAllProducts, findOneProduct, priceCalculator} = require("../../controllers/apis/productReactApi")
 
 router.get('/api/lastProducts', findLastFiveProducts)
 router.get('/api/products', findAllProducts)
 router.get('/api/products/:id', findOneProduct)
+router.get("/api/products/priceCalculator",priceCalculator)
+
+module.exports = router
