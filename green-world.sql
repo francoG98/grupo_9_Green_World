f15601189733d232225a87757bcfbf3bc2e8f4b2
“httpini” <nicolas.pinol@hotmail.com>
2022-10-04 21:23:24 -0300

    Subo la base actualizada a hoy

--- green-world.sql
+++ green-world.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-09-2022 a las 02:35:09
+-- Tiempo de generación: 05-10-2022 a las 01:51:37
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -90,7 +90,18 @@ INSERT INTO `imagenes` (`id`, `path`) VALUES
 (28, 'VERMICULITA-GO-GREEN.png'),
 (29, 'FILTRO-6-COOLER-B1.png'),
 (30, 'TOBERA-COOLER-4a4.png'),
-(31, 'MALLA-SCROG-SEGURIDAD-B.png');
+(31, 'MALLA-SCROG-SEGURIDAD-B.png'),
+(32, 'image_id-1663535700100-581360151.jfif'),
+(33, 'image_id-1663535778646-843299933.jfif'),
+(34, 'image_id-1663612733033-151232062.jfif'),
+(35, 'image_id-1663639387772-738064049.jfif'),
+(36, 'image_id-1664652728247-982614596.jfif'),
+(37, 'image_id-1664689777155-383901123.jfif'),
+(38, 'avatar-1664696428122-596860618.jpg'),
+(39, 'avatar-1664696796578-579988858.jfif'),
+(40, 'image_id-1664778102314-370162511.jpg'),
+(41, 'image_id-1664778289174-424162161.jfif'),
+(42, 'image_id-1664779070137-818417655.jfif');
 
 -- --------------------------------------------------------
 
@@ -118,7 +129,7 @@ INSERT INTO `productos` (`id`, `name`, `description`, `image_id`, `category_id`,
 (6, 'Pipas de cristal', 'La Pipa de Cristal Champ High de 11 cm está fabricada en cristal de borosilicato de uso médico con alta resistencia térmica para conseguir una calada fresca y limpia. Cuenta con carb (carburador) para vaciar la pipa. Su forma redondeada resulta perfecta para llevar en un bolsillo.Disponible en colores azul, rosa, transparente y verde.', 18, 4, NULL, 'Dorado', 800),
 (7, 'Celulosa', 'Papel de celulosa Zeus de color naranja es un papel naranja fabricado a partir de celulosa 100% natural. Esta presentación de celulosa Zeus no produce ceniza ni tampoco necesita pegamento, ya que humedeciendo uno de sus extremos, el cigarrillo queda prolijamente cerrado. Las celulosas vienen de color Naranja lo que le da un toque estético único.', 19, 4, NULL, NULL, 300),
 (8, 'Aceite Cannabico', 'Aceite hidratante corporal que ayuda a la relajación.', 20, 3, NULL, NULL, 3500),
-(9, 'Crema de cannabis Kif Skin Control 60grs', 'Este es un producto que cuenta con todas las propiedades cosméticas del cannabis ya que contiene extracto de la flor, y por ende es un producto de última tendencia en el mercado nacional. Es una novedosa formulación cosmética con todos los beneficios del cannabis (CBD) y la caléndula para el cuidado de la piel, con propiedades antioxidantes, ayuda a combatir las manchas y marcas producidas por el acné para obtener una piel más sana y lúcida.', 21, 3, NULL, NULL, 1500),
+(9, 'Crema de cannabis Kif Skin Control', 'Este es un producto que cuenta con todas las propiedades cosméticas del cannabis ya que contiene extracto de la flor, y por ende es un producto de última tendencia en el mercado nacional. Es una novedosa formulación cosmética con todos los beneficios del cannabis (CBD) y la caléndula para el cuidado de la piel, con propiedades antioxidantes, ayuda a combatir las manchas y marcas producidas por el acné para obtener una piel más sana y lúcida.\r\nPeso: 60grs', 21, 3, NULL, NULL, 1500),
 (10, 'Aceite Cannabico 30ml', 'El aceite de cannabis es a base de oliva extravirgen con extracto de cannabis full spectrum, contiene aproximadamente entre 2.75% de CBD y menos del 1% de THC.', 22, 3, NULL, NULL, 5000),
 (11, 'Top Veg', 'Top Crop es un fertilizante líquido completo rico en ácidos húmicos y fúlvicos así como en macro y micronutrientes solubles en agua.', 23, 2, NULL, NULL, 1000),
 (12, 'Go-nitro-300x300', 'Go Green Argentina es un bio-fertilizante totalmente orgánico y natural indicado para el periodo vegetativo de tus plantas, que representa una estable e inigualable fuente de nitrógeno «saludable».', 24, 2, NULL, NULL, 1000),
@@ -153,9 +164,14 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `name`, `lastname`, `email`, `cultivo`, `password`, `image_id`, `admin`, `product_id`) VALUES
-(1, 'Inosuke', 'Hashibira', 'ihashibira@gworld.com', 'outdoor', '$2a$10$dwIiGPKgYnE1VwscsmSCQeMUX2lDWkngpHns1fEp7WZj7vPgzrMye', 7, 1, NULL),
-(2, 'Eric', 'Zatel', 'ezatel@gworld.com', 'ninguna', '$2a$10$dwIiGPKgYnE1VwscsmSCQeMUX2lDWkngpHns1fEp7WZj7vPgzrMye', 6, 1, NULL),
-(4, 'Nico', 'Pini', 'nico.pini@email.com', 'ninguna', '$2a$10$GxYV/YruCgUuQNxRRWJNd.lH1aMwuLjmCUmJQ4vVdhQZqUkLgc8r6', 6, 0, NULL);
+(11, 'Gon', 'Freecs', 'gfreecs@gworld.com', 'indoor', '$2a$10$8.igHy/jRNQER23wCelt1eL80NXUGnFY6S6SgO34X7jL8wc/G5cVK', 35, 1, NULL),
+(12, 'Killua', 'Zoldyck', 'kzoldyck@hxh.com', 'outdoor', '$2a$10$PcWbVB/hp72OEHG9lUeKKe2XNVhMQSmgl87KS4h4FoNERv9bnyq/2', 36, 0, NULL),
+(13, 'Justin', 'Bieber', 'justin@bieber.com', 'ninguna', '$2a$10$7rCkKLerHHPHVbUMx3N9yeFPMMsq0y6lYQ95mh9L/Gt3Af58TAsem', 37, 0, NULL),
+(14, 'Ramita', 'Superheroe', 'ramita@gworld.com', 'outdoor', '$2a$10$rE7rpmS71I.D4TYjLnz1vuUIOE4uXbcRMaAQp6H8VUaa83yCx33Im', 38, 1, NULL),
+(15, 'Mike', 'Tyson', 'mtyson@gworld.com', 'hidro', '$2a$10$7JUIoGsrOK4qwDAcGd6YwuAHQlAlOWlW6hDal/qzY.Fkc/l/uWqk6', 39, 1, NULL),
+(16, 'Snoop', 'Dogg', 'calvincb@g.com', 'indoor', '$2a$10$GN64lEWC1LehKGoxadZB2eoSIvsA92r4261//iGSHehbfv71074Ka', 40, 0, NULL),
+(17, 'Inosuke', 'Hashibira', 'ihashibira@gworld.com', 'outdoor', '$2a$10$kt4B2TscteNveSsA3FoCAeJOqLqZJxiJRWMjiR57swHNPnirKu4WG', 41, 1, NULL),
+(18, 'Andy', 'Chango', 'afejerman@hotmail.com', 'hidro', '$2a$10$fI/.DlZ8HK21YfGLcvkHlO/Itct4oJagqBH1BV7pFlBx8xw9zFsiS', 42, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -227,7 +243,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -239,7 +255,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `usuariosproductos`
